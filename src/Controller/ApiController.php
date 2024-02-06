f39242bff62f5a2597fb9353c0d232e35a243c5d
jfsenechal <jfsenechal@gmail.com>
2024-02-06 10:33:42 +0100

    up sf 7.0

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,7 +5,7 @@ namespace AcMarche\Taxe\Controller;
 use AcMarche\Taxe\Repository\NomenclatureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace AcMarche\Taxe\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
