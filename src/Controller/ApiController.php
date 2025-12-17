aa57011eb4f6015d66c7915627056931449b976f
jfsenechal <jfsenechal@gmail.com>
2025-12-17 10:12:21 +0100

    refactor ApiController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,13 +6,11 @@ use AcMarche\Taxe\Repository\NomenclatureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
     public function __construct(
         private readonly NomenclatureRepository $nomenclatureRepository,
-        private readonly SerializerInterface $serializer
     ) {
     }
 
@@ -39,8 +37,8 @@ class ApiController extends AbstractController
                     'position' => $taxe->getPosition(),
                     'exercices' => $exercices,
                 ];
-                $data[] = $row;
             }
+            $data[] = $row;
         }
 
         return new JsonResponse($data);
